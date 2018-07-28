7750dc31cf49c4e5384f613be805467f313de4e3
Sachin Goyal <goyalsachin22@gmail.com>
2018-07-29 00:59:31 +0530

    Ouput Properties and Custom events and ng-container example

--- Angular2Demo/Angular2Demo/src/app/employee/employeeList.component.js
+++ Angular2Demo/Angular2Demo/src/app/employee/employeeList.component.js
@@ -12,6 +12,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var EmployeeListComponent = /** @class */ (function () {
     function EmployeeListComponent() {
+        this.selectedEmployeeCountRadioButton = 'All';
         this.employees = [
             { code: 'emp101', name: 'Sachin', gender: 'Male', annualSalary: 5500, DOB: '04/22/1988' },
             { code: 'emp102', name: 'Krati Jaitwal', gender: 'Female', annualSalary: 7500, DOB: '04/24/1991' }
@@ -20,12 +21,26 @@ var EmployeeListComponent = /** @class */ (function () {
     EmployeeListComponent.prototype.getEmployees = function () {
         this.employees = [
             { code: 'emp101', name: 'Sachin', gender: 'Male', annualSalary: 5500, DOB: '04/22/1988' },
-            { code: 'emp102', name: 'Krati Jaitwal', gender: 'Female', annualSalary: 7500, DOB: '04/24/1991' },
-            { code: 'emp103', name: 'Prateek Jaitwal', gender: 'Male', annualSalary: 10000, DOB: '01/31/1988' }
+            { code: 'emp102', name: 'Krati Jaitwal', gender: 'Female', annualSalary: 7500, DOB: '04/24 /1991' },
+            { code: 'emp103', name: 'Prateek Jaitwal', gender: 'Male', annualSalary: 10000, DOB: '01/31/1988' },
+            { code: 'emp104', name: 'Kartik Kumar', gender: 'Male', annualSalary: 20000, DOB: '01/27/1990' },
+            { code: 'emp104', name: 'Adyasha Pruti', gender: 'Female', annualSalary: 15700, DOB: '04/23/1993' },
         ];
     };
     EmployeeListComponent.prototype.trackByEmpCode = function (index, employee) {
         return employee.code;
+    };
+    EmployeeListComponent.prototype.getTotalEmployeesCount = function () {
+        return this.employees.length;
+    };
+    EmployeeListComponent.prototype.getTotalMaleEmployeesCount = function () {
+        return this.employees.filter(function (x) { return x.gender === "Male"; }).length;
+    };
+    EmployeeListComponent.prototype.getTotalFemaleEmployeesCount = function () {
+        return this.employees.filter(function (x) { return x.gender === "Female"; }).length;
+    };
+    EmployeeListComponent.prototype.onEmployeeCountRadioButtonChange = function (selectedRadioButtonValue) {
+        this.selectedEmployeeCountRadioButton = selectedRadioButtonValue;
     };
     EmployeeListComponent = __decorate([
         core_1.Component({
--- Angular2Demo/Angular2Demo/src/app/employee/employeeList.component.ts
+++ Angular2Demo/Angular2Demo/src/app/employee/employeeList.component.ts
@@ -18,12 +18,29 @@ export class EmployeeListComponent {
     getEmployees(): void {
         this.employees = [
             { code: 'emp101', name: 'Sachin', gender: 'Male', annualSalary: 5500, DOB: '04/22/1988' },
-            { code: 'emp102', name: 'Krati Jaitwal', gender: 'Female', annualSalary: 7500, DOB: '04/24/1991' },
-            { code: 'emp103', name: 'Prateek Jaitwal', gender: 'Male', annualSalary: 10000, DOB: '01/31/1988' }
+            { code: 'emp102', name: 'Krati Jaitwal', gender: 'Female', annualSalary: 7500, DOB: '04/24 /1991' },
+            { code: 'emp103', name: 'Prateek Jaitwal', gender: 'Male', annualSalary: 10000, DOB: '01/31/1988' },
+            { code: 'emp104', name: 'Kartik Kumar', gender: 'Male', annualSalary: 20000, DOB: '01/27/1990' },
+            { code: 'emp104', name: 'Adyasha Pruti', gender: 'Female', annualSalary: 15700, DOB: '04/23/1993' },
         ];
     }
 
     trackByEmpCode(index: number, employee: any): string {
         return employee.code;
+    }
+
+    getTotalEmployeesCount(): number {
+        return this.employees.length;
+    }
+    getTotalMaleEmployeesCount(): number {
+        return this.employees.filter(x => x.gender === "Male").length;
+    }
+    getTotalFemaleEmployeesCount(): number {
+        return this.employees.filter(x => x.gender === "Female").length;
+    }
+
+    selectedEmployeeCountRadioButton: string = 'All';
+    onEmployeeCountRadioButtonChange(selectedRadioButtonValue: string): void {
+        this.selectedEmployeeCountRadioButton = selectedRadioButtonValue;
     }
 }
