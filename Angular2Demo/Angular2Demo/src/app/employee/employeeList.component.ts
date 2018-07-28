be8f04252adaf885f233056ef03a4e1075d6179e
Sachin Goyal <goyalsachin22@gmail.com>
2018-07-29 01:30:39 +0530

    Interfaces

--- Angular2Demo/Angular2Demo/src/app/employee/employeeList.component.ts
+++ Angular2Demo/Angular2Demo/src/app/employee/employeeList.component.ts
@@ -1,4 +1,5 @@
 ﻿import { Component } from '@angular/core';
+import { IEmployee } from './Employee'
 
 @Component({
     selector: 'list-employee',
@@ -6,7 +7,7 @@
     styleUrls: ['app/employee/employeeList.component.css']
 })
 export class EmployeeListComponent {
-    employees: any[];
+    employees: IEmployee[];
 
     constructor() {
         this.employees = [
